a0fd3ff85a413b20a999e0ef3cc64e5ea5828fec
Guilherme O <guilherme_otran@hotmail.com>
2016-03-24 21:41:16 -0300

    detecta entradas malvadas; pilha dinamica

--- maze/main.c
+++ maze/main.c
@@ -2,7 +2,14 @@
 #include <stdlib.h>
 #include <locale.h> //Para idioma pt-br
 
-#define QTD 100
+#define FALSE 0
+#define TRUE 1
+typedef int bool;
+
+#define MAZE_WALL 'p'
+#define MAZE_IN 'e'
+#define MAZE_OUT 's'
+#define MAZE_WAY 'b'
 
 struct point{
     int x;
@@ -10,11 +17,11 @@ struct point{
 };
 typedef struct point Point;
 
-struct pilha{
-    Point p[QTD];
+struct stack{
+    Point *p;
     int topo;
 };
-typedef struct pilha Pilha;
+typedef struct stack Stack;
 
 struct boolPos {
     int left;
@@ -24,66 +31,76 @@ struct boolPos {
 };
 typedef struct boolPos BoolPos;
 
+// Stack
+Stack* createStack(int size);
+bool pop(Stack *p1, Point *point);
+void push(Stack *p1, Point w);
+void deinitStack(Stack *s);
 
-Point pop(Pilha *p1);
-int push(Pilha *p1, Point w);
-Point searchEntry(int l, int c, char m[][c]);
+// Helpers
+bool searchEntry(int l, int c, char m[][c], Point *point);
 void checkPositions(BoolPos *s, Point *c, int lin, int col, char maze[][col]);
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
-    FILE *arquivo;
     int lin, col;
-    char **maze;
     Point pos;
-    Pilha ways;
-    ways.topo = 0;
+    Stack *ways;
     BoolPos currentSituation;
     int i, j;
+
+    FILE *arquivo;
     arquivo = fopen("labirinto.txt", "r");
-    if(arquivo == NULL)
+
+    if(arquivo == NULL) {
         printf("Erro ao carregar o labirinto, arquivo não pode ser aberto.");
-    else {
-        fscanf(arquivo, "%i %i", &lin, &col);
-        printf("Labirinto %i x %i\n\n", lin, col);
+        return 0;
+    }
 
-        char (*maze)[col] = (char(*)[col]) malloc(sizeof(char)*lin*col);
+    fscanf(arquivo, "%i %i", &lin, &col);
+    printf("Labirinto %i x %i\n\n", lin, col);
 
-        for(i=0; i<lin; i++)
-        {
-            fscanf(arquivo, "%s", maze[i]);
+    char (*maze)[col] = (char(*)[col]) malloc(sizeof(char)*lin*col);
+
+    for(i=0; i<lin; i++) {
+        fscanf(arquivo, "%s", maze[i]);
+    }
+
+    if (searchEntry(lin, col, maze, &pos) == FALSE) {
+        printf("\nNão há entrada para esse labirinto, impossível continuar");
+        return 0;
+    }
+
+    ways = createStack(lin * col);
+    push(ways, pos);
+
+    while(maze[pos.x][pos.y] != MAZE_OUT) {
+        checkPositions(&currentSituation, &pos, lin, col, maze);
+
+        if(maze[pos.x][pos.y] == MAZE_WAY) {
+            maze[pos.x][pos.y] = 'v'; //Move
         }
 
-        pos = searchEntry(lin, col, maze);
+        printf("\nTentativa: %i x %i", pos.x, pos.y);
 
-        if(pos.x == -1 && pos.y == -1)
-            printf("\nNão há entrada para esse labirinto, impossível continuar");
-        else {
-            push(&ways, pos);
-        }
-        while(maze[pos.x][pos.y] != 's') {
-            checkPositions(&currentSituation, &pos, lin, col, maze);
-            if(maze[pos.x][pos.y] == 'b')
-                maze[pos.x][pos.y] = 'v'; //Move
-            printf("\nTentativa: %i x %i", pos.x, pos.y);
-            if(currentSituation.left == 0 && currentSituation.bottom == 0 && currentSituation.right == 0 && currentSituation.top == 0) {
-                pos = pop(&ways);
+        if(currentSituation.left == 0 && currentSituation.bottom == 0 && currentSituation.right == 0 && currentSituation.top == 0) {
+            if (pop(ways, &pos) == FALSE) {
+                printf("Entrada encontrada não leva para uma saída!\n");
+                return 0;
             }
-            else
-                push(&ways, pos);
-        }
-        if(maze[pos.x][pos.y] == 's') {
-            printf("\n\nLabirinto descoberto com sucesso!\nO caminho é:\n\n");
-            while(ways.topo > 0) {
-                pos = pop(&ways);
-                printf("%iº passo: %i x %i\n", ways.topo +1, pos.x, pos.y);
-            }
-            printf("\n\n");
-        }
-        else {
-            printf("Não foi possível descobrir o caminho deste labirinto\n\n");
+        } else {
+            push(ways, pos);
         }
     }
+
+    printf("\n\nLabirinto descoberto com sucesso!\nO caminho é:\n\n");
+
+    while(pop(ways, &pos) == TRUE) {
+        printf("%iº passo: %i x %i\n", ways->topo +1, pos.x, pos.y);
+    }
+
+    deinitStack(ways);
+    printf("\n\n");
 
     free(maze);
     fclose(arquivo);
@@ -92,75 +109,100 @@ int main() {
     return 0;
 }
 
-//Functions
-Point pop(Pilha *p1) {
-    Point r;
+// Stack Functions
+Stack* createStack(int size) {
+    Stack *s = (Stack*) malloc(sizeof(Stack));
+    s->p = (Point*) malloc(sizeof(Point) * size);
+    s->topo = 0;
 
-    if(p1->topo == 0) {
-        r.x = -1;
-        r.y = -1;
-    }
-    else {
-        p1->topo--;
-        r.x = p1->p[p1->topo].x;
-        r.y = p1->p[p1->topo].y;
-    }
-    return r;
+    return s;
 }
-int push(Pilha *p1, Point w) {
-    if(p1->topo == QTD)
-        return 0;
+
+bool pop(Stack *p1, Point *point) {
+    if(p1->topo == 0) {
+        return FALSE;
+    }
+
+    p1->topo--;
+    point->x = p1->p[p1->topo].x;
+    point->y = p1->p[p1->topo].y;
+
+    return TRUE;
+}
+
+void push(Stack *p1, Point w) {
     p1->p[p1->topo].x = w.x;
     p1->p[p1->topo].y = w.y;
     p1->topo++;
-    return 1;
 }
 
-Point searchEntry(int l, int c, char m[][c]) {
-    Point e;
-    int i, j;
-    e.x = -1;
-    e.y = -1;
-    for(i=0; i<l; i++) {
-        for(j=0; j<c; j++)
-        {
-            printf("%c ", m[i][j]);
+void deinitStack(Stack *s) {
+    free(s->p);
+    free(s);
+}
 
-            if(m[i][j] == 'e') {
-                e.x = i;
-                e.y = j;
+// Helpers
+bool searchEntry(int l, int c, char m[][c], Point *point) {
+    int i, j;
+    bool found = FALSE;
+    char current;
+
+    for(i=0; i<l; i++) {
+        for(j=0; j<c; j++) {
+            current = m[i][j];
+
+            if(current == MAZE_IN && found == FALSE) {
+                point->x = i;
+                point->y = j;
+                found = TRUE;
+                printf("%c", toupper(current));
+            } else {
+                switch (current) {
+                    case MAZE_WALL:
+                        printf("#");
+                        break;
+                    case MAZE_OUT:
+                        printf("S");
+                        break;
+                    case MAZE_IN:
+                        printf("e");
+                        break;
+                    default:
+                        printf(" ");
+                        m[i][j] = MAZE_WAY;
+                        break;
+                }
             }
         }
         printf("\n");
     }
-    return e;
-}
 
+    return FALSE;
+}
 
 void checkPositions(BoolPos *s, Point *c, int lin, int col, char maze[][col]){
     s->left = 0;
     s->bottom = 0;
     s->right = 0;
     s ->top = 0;
-    if((c->y + 1 <= lin) && (maze[c->x][c->y+1] == 'b' || maze[c->x][c->y+1] == 's' )) {
+
+    if((c->y + 1 <= lin) && (maze[c->x][c->y+1] == MAZE_WAY || maze[c->x][c->y+1] == MAZE_OUT )) {
         s->left = 1;
         c->y += 1;
         return;
     }
-    if((c->x + 1 <= col) && (maze[c->x +1][c->y] == 'b'|| maze[c->x+1][c->y] == 's')) {
+    if((c->x + 1 <= col) && (maze[c->x +1][c->y] == MAZE_WAY|| maze[c->x+1][c->y] == MAZE_OUT)) {
         s->bottom = 1;
         c->x += 1;
         return;
     }
-    if((c->y - 1 >= 0) && (maze[c->x][c->y-1] == 'b' || maze[c->x][c->y-1] == 's')) {
+    if((c->y - 1 >= 0) && (maze[c->x][c->y-1] == MAZE_WAY || maze[c->x][c->y-1] == MAZE_OUT)) {
         s->right = 1;
         c->y -= 1;
         return;
     }
-    if((c->x -1 >= 0) && (maze[c->x-1][c->y] == 'b') || maze[c->x-1][c->y] == 's') {
+    if((c->x -1 >= 0) && (maze[c->x-1][c->y] == MAZE_WAY) || maze[c->x-1][c->y] == MAZE_OUT) {
         s->top = 1;
         c->x -= 1;
     }
 }
-
-
